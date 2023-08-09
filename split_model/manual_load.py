2f39aafdb1d5086cc18c24fbd3b488d1c46e8b27
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2023-08-08 22:13:06 -0400

    split model: manual torch pickle load

---
+++ split_model/manual_load.py
@@ -0,0 +1,31 @@
+import pickle
+import sys
+import collections
+
+def load_torch_pickle(fpath):
+    class DummyObj:
+        def __init__(self, *args):
+            pass
+
+    class DummyUnpickler(pickle.Unpickler):
+        def find_class(self, module, name):
+            print(f'custom_find_class {module} {name}')
+            if name == 'OrderedDict':
+                return collections.OrderedDict
+            return DummyObj
+
+    with open(fpath, 'rb') as f:
+        def persistent_load(saved_id):
+            print(f'persistent_load {saved_id}')
+            typename, storage_type, key, location, numel = saved_id
+            return None
+        
+        unpickler = DummyUnpickler(f)
+
+        unpickler.persistent_load = persistent_load
+
+        result = unpickler.load()
+        print(result)
+
+
+load_torch_pickle(sys.argv[1])
