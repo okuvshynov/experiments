319f9ff5f80c40e847eb461620758c7e29aaa5d0
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2023-08-09 13:35:48 -0400

    split model: load llama2_7b manually

--- split_model/manual_load.py
+++ split_model/manual_load.py
@@ -8,7 +8,12 @@ import numpy as np
 import json
 
 # use ones from llama2.c
+sys.path.insert(0, '../llama2.c')
 from model import Transformer, ModelArgs
+
+
+# sys.argv[1] - path to consolidated.00.pth
+# sys.argv[2] - path to params.json
 
 def load_torch_pickle(file):
     class DummyObj:
@@ -34,7 +39,7 @@ def load_torch_pickle(file):
     return list(result.keys())
 
 
-def load_manual_state_dict():
+def load_state_dict():
     manual_state_dict = OrderedDict()
 
     with zipfile.ZipFile(sys.argv[1]) as checkpoint_zip:
@@ -44,25 +49,15 @@ def load_manual_state_dict():
         for i, module_name in enumerate(module_list):
             print(i, module_name)
             with checkpoint_zip.open(f'consolidated/data/{i}', 'r') as data_file:
+                # TODO: is it float16 of bfloat16?
                 data = np.frombuffer(data_file.read(), dtype=np.float16)
             manual_state_dict[module_name] = torch.tensor(data)
             print(module_name, manual_state_dict[module_name].shape)
+    return manual_state_dict
 
 
-def torch_module_dfs(module, prefix=''):
-    res = []
-    if hasattr(module, 'weight'):
-        res.append((f'{prefix}weight', module.weight.shape))
-        print(f'{prefix}weight -- {module}')
-    for name, child in module._modules.items():
-        if child is not None:
-            child_prefix = prefix + name + '.'
-            res.extend(torch_module_dfs(child, child_prefix))
-    return res
 
-#
-
-def load_llama7b_torch():
+def load_llama7b():
     vocab_size = 32000
     with open(sys.argv[2], 'r') as conf_file:
         config = json.loads(conf_file.read())
@@ -72,14 +67,45 @@ def load_llama7b_torch():
     model = Transformer(model_conf)
     return model
 
-def run_llama7b_torch():
-    torch_state_dict = torch.load(sys.argv[1])
+
+## loading with torch (default mode)
+def llama7b_torch():
+    model = load_llama7b()
+
+    state_dict = torch.load(sys.argv[1])
     
     # model definition is a little different
-    del torch_state_dict['rope.freqs']
-    #print(f"rope.freqs from state_dict: {torch_state_dict['rope.freqs']}")
-    model = load_llama7b_torch()
-    model.load_state_dict(torch_state_dict)
+    del state_dict['rope.freqs']
+    
+    model.load_state_dict(state_dict)
     print(model)
 
-run_llama7b_torch()
+
+## loading manually
+def llama7b_manual():
+    model = load_llama7b()
+
+    state_dict = load_state_dict()
+
+    def fix_shapes_rec(module, prefix=''):
+        nonlocal state_dict
+        if hasattr(module, 'weight'):
+            key = f'{prefix}weight'
+            loaded_shape = state_dict[key].shape
+            print(f'found {key} with shapes {module.weight.shape} and {loaded_shape}.')
+            state_dict[key] = state_dict[key].reshape(module.weight.shape)
+
+        for name, child in module._modules.items():
+            if child is not None:
+                child_prefix = prefix + name + '.'
+                fix_shapes_rec(child, child_prefix)
+
+    fix_shapes_rec(model)
+
+    # model definition is a little different
+    del state_dict['rope.freqs']
+
+    model.load_state_dict(state_dict)
+    print(model)
+
+llama7b_manual()
