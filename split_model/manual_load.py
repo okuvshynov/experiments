a780ef0ebd5b96510df05320b5e3c01aaef96508
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2023-08-10 13:39:06 -0400

    split model: better printing

--- split_model/manual_load.py
+++ split_model/manual_load.py
@@ -106,7 +106,7 @@ def llama7b_phantom():
         
     # now we need to populate everything except layers using strict=False
     remaining_modules = [(j, k) for j, k in enumerate(modules) if not k.startswith('layers.')]
-    print(remaining_modules)
+    #print(remaining_modules)
 
     state_dict = populate_phantom_state_dict(remaining_modules)
     def fix_shapes_rec(module, prefix=''):
@@ -140,12 +140,13 @@ model = llama7b_phantom().to(device)
 print(f'loaded phantom model in {time.time() - start} seconds')
 start = time.time()
 phantom_y = model(X)
+torch.set_printoptions(profile="full", precision=10)
 print(phantom_y)
 print(f'evaluated phantom model in {time.time() - start} seconds')
 
 
 """
-This produces RuntimeError: MPS backend out of memory:
+trying to put whole model to mps produces RuntimeError: MPS backend out of memory:
 model = llama7b_torch().to('mps')
 print(model(X))
 """
@@ -163,11 +164,21 @@ same_y = torch.allclose(phantom_y.cpu(), default_y.cpu())
 print(f'output for default torch model and phantom model is same: {same_y}')
 
 
-# Output from Apple M2 @ 24Gb RAM running on CPU:
+# Output from Apple M2 @ 24Gb RAM running on MPS and CPU:
 """
-loaded model in 93.34452700614929 seconds
+% python split_model/manual_load.py ../llama-2-7b/consolidated.00.pth ../llama-2-7b/params.json
+loaded phantom model in 88.39778780937195 seconds
+tensor([[[-7.7749,  0.2068,  3.7988,  ..., -2.6116, -3.5703, -1.7604]]],
+       device='mps:0', grad_fn=<LinearBackward0>)
+evaluated phantom model in 13.354475975036621 seconds
+loaded model in 93.51274299621582 seconds
 tensor([[[-7.7749,  0.2068,  3.7988,  ..., -2.6116, -3.5703, -1.7604]]],
        grad_fn=<UnsafeViewBackward0>)
-evaluated model in 132.63551712036133 seconds
+evaluated model in 129.28788685798645 seconds
+output for default torch model and phantom model is same: False
 
 """
+
+# Looks like the output is not exactly the same but very close - 
+# I wonder if it's device/precision thing. 
+# Let's check with running both on CPU  
