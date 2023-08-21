42dcd90251c4004c76c53825e19012208f8e4917
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2023-08-20 21:17:44 -0400

    split model: simplify test_backprop

--- split_model/test_backprop.py
+++ split_model/test_backprop.py
@@ -20,6 +20,14 @@ lr = 100.0
 model_path = sys.argv[1]
 mode = sys.argv[2] if len(sys.argv) > 2 else 'data'
 
+test_data_paths = [
+    'split_model/test_data/sample_weights_before.pt',
+    'split_model/test_data/sample_weights_after.pt',
+    'split_model/test_data/logits.pt',
+    'split_model/test_data/sample_emb_before.pt',
+    'split_model/test_data/sample_emb_after.pt'
+]
+
 txt = lambda ok: '[ OK ]' if ok else '[FAIL]'
 
 def blackbox_backwards(device='cpu'):
@@ -89,78 +97,23 @@ def plain_backwards(device='cpu'):
     emb_after = model.tok_embeddings.weight[:test_data_dim, :test_data_dim].clone()
     return weight_before, weight_after, logits[0, :length, :test_data_dim].clone(), emb_before, emb_after
 
-def reference_compare(save_test_data=False):
-    print('Running blackbox on CPU')
-    wb, wa, y, emb_before, emb_after = blackbox_backwards('cpu')
+def get_comparison_data():
+    return plain_backwards() if mode == 'ref' else tuple(torch.load(p) for p in test_data_paths)
 
-    print('Running plain on CPU')
-    wb_plain, wa_plain, y_plain, emb_before_plain, emb_after_plain  = plain_backwards()
+def run(save_test_data):
+    wb, wa, y, emb_before, emb_after = blackbox_backwards('cpu')
+    comparison_data = get_comparison_data()
     if save_test_data:
-        torch.save(wb_plain, 'split_model/test_data/sample_weights_before.pt')
-        torch.save(wa_plain, 'split_model/test_data/sample_weights_after.pt')
-        torch.save(emb_before_plain, 'split_model/test_data/sample_emb_before.pt')
-        torch.save(emb_after_plain, 'split_model/test_data/sample_emb_after.pt')
-        torch.save(y_plain, 'split_model/test_data/logits.pt')
+        for t, p in zip(comparison_data, test_data_paths):
+            torch.save(t, p)
 
-    same_before = torch.allclose(wb.cpu(), wb_plain.cpu())
-    same_after = torch.allclose(wa.cpu(), wa_plain.cpu())
-    same_y = torch.allclose(y.cpu(), y_plain.cpu())
-    same_emb_before = torch.allclose(emb_before.cpu(), emb_before_plain.cpu())
-    same_emb_after = torch.allclose(emb_after.cpu(), emb_after_plain.cpu())
-
-    print(f'{txt(same_before)} weights before')
-    print(f'{txt(same_after)} weights after')
-    print(f'{txt(same_emb_before)} emb weights before')
-    print(f'{txt(same_emb_after)} emb weights after')
-    print(f'{txt(same_y)} out logits')
-
-def test_data_compare():
-    print('Running on CPU')
-    wb, wa, y, emb_before, emb_after = blackbox_backwards('cpu')
-
-    wb_plain = torch.load('split_model/test_data/sample_weights_before.pt')
-    wa_plain = torch.load('split_model/test_data/sample_weights_after.pt')
-    y_plain = torch.load('split_model/test_data/logits.pt')
-    emb_before_plain = torch.load('split_model/test_data/sample_emb_before.pt')
-    emb_after_plain = torch.load('split_model/test_data/sample_emb_after.pt')
-
-    same_before = torch.allclose(wb.cpu(), wb_plain.cpu())
-    same_after = torch.allclose(wa.cpu(), wa_plain.cpu())
-    same_y = torch.allclose(y.cpu(), y_plain.cpu())
-    same_emb_before = torch.allclose(emb_before.cpu(), emb_before_plain.cpu())
-    same_emb_after = torch.allclose(emb_after.cpu(), emb_after_plain.cpu())
+    wb_plain, wa_plain, y_plain, emb_before_plain, emb_after_plain = comparison_data
     
-    print(f'{txt(same_before)} weights before')
-    print(f'{txt(same_after)} weights after')
-    print(f'{txt(same_emb_before)} emb weights before')
-    print(f'{txt(same_emb_after)} emb weights after')
-    print(f'{txt(same_y)} out logits')
+    print(f'{txt(torch.allclose(wb.cpu(), wb_plain.cpu()))} weights before')
+    print(f'{txt(torch.allclose(wa.cpu(), wa_plain.cpu()))} weights after')
+    print(f'{txt(torch.allclose(emb_before.cpu(), emb_before_plain.cpu()))} emb weights before')
+    print(f'{txt(torch.allclose(emb_after.cpu(), emb_after_plain.cpu()))} emb weights after')
+    print(f'{txt(torch.allclose(y.cpu(), y_plain.cpu()))} out logits')
 
 if __name__ == '__main__':
-    #backprop_service.lr = lr
-    if mode == 'data':
-        test_data_compare()
-    else:
-        reference_compare()
-
-
-"""
-python split_model/backward_cmp.py ../llama-2-7b
-Running on CPU
-Loaded 292 module metadata
-Created blank model
-processing transformer blocks ................................ DONE
-populated all weights to model
-loaded model in 89.64134502410889 seconds
-forward pass in 33.529903173446655 seconds
-backward pass in 129.32752394676208 seconds
-Running plain on CPU
-loaded plain model in 96.33211588859558 seconds
-torch.Size([32, 50, 32000]) torch.Size([1600, 32000]) torch.Size([1600])
-plain forward pass in 151.54023575782776 seconds
-plain backward pass in 210.3796899318695 seconds
-[ OK ] weights before
-[ OK ] weights after
-[ OK ] out logits
-
-"""
+    run(save_test_data=False)
