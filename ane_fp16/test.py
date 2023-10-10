c181feb596ee63b5433c3a593a80a413137aa649
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2023-10-10 12:01:27 -0400

    exp: mean

--- ane_fp16/test.py
+++ ane_fp16/test.py
@@ -14,6 +14,4 @@ b16 = torch.rand((dim, dim)).to('mps').to(torch.float16)
 
 c16 = a16 @ b16
 
-diff = torch.abs(c32 - c16.to(torch.float32)).view(-1).to('cpu')
-q = torch.tensor([0.5, 0.95, 0.99])
-print(torch.quantile(diff, q))
+print(torch.mean(torch.abs(c32 - c16.to(torch.float32))))
