cf7813e9b243bb4bd583a136422d8bb013d95b56
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2024-05-08 11:06:07 -0400

    simplify speculator

--- dlm/speculator.cpp
+++ dlm/speculator.cpp
@@ -10,7 +10,6 @@
 #include <zmq.hpp>
 
 #include "config.h"
-#include "query_context.h"
 #include "utils.h"
 
 namespace
@@ -18,67 +17,19 @@ namespace
 
 using json = nlohmann::json;
 
-// instead of receiving a copy of request it should 'attach' itself to the main node?
-class speculator
-{
-  public:
-    static std::unique_ptr<speculator> create(config conf);
-    virtual ~speculator();
-    int loop();
-
-  private:
-    explicit speculator(config conf);
-
-    bool call(llama_tokens & curr, size_t & n_matched);
-
-    llama_model   * model_;
-    const config    conf_;
-
-    // current context, as we operate on one query at a time
-    query_context query_ctx_;
-};
-
-std::unique_ptr<speculator> speculator::create(config conf)
-{
-    auto self = std::unique_ptr<speculator>(new speculator(conf));
-
-    llama_model_params model_params = llama_model_default_params();
-    model_params.n_gpu_layers       = conf.n_gpu_layers;
-
-    self->model_ = llama_load_model_from_file(conf.model_path.c_str(), model_params);
-
-    if (self->model_ == nullptr)
-    {
-        fprintf(stderr, "Model %s load failed.\n", conf.model_path.c_str());
-        return nullptr;
-    }
-
-    return self;
-}
-
-speculator::speculator(config conf): conf_(conf)
-{
-}
-
-speculator::~speculator()
-{
-    if (model_ != nullptr)
-    {
-        llama_free_model(model_);
-    }
-}
+using llama_tokens = std::vector<llama_token>;
 
 // returns true if main model completed the generation
-bool speculator::call(llama_tokens & curr, /* OUT */ size_t & n_matched)
+bool call(zmq::socket_t * client, llama_tokens & curr, /* OUT */ size_t & n_matched)
 {
     json req_j;
     req_j["spec"] = curr;
 
     auto req_z = json_to_zmsg(req_j);
-    query_ctx_.client->send(req_z, zmq::send_flags::none);
+    client->send(req_z, zmq::send_flags::none);
 
     zmq::message_t res_z;
-    query_ctx_.client->recv(res_z, zmq::recv_flags::none);
+    client->recv(res_z, zmq::recv_flags::none);
 
     auto res_j = json_from_zmsg(res_z);
 
@@ -92,29 +43,38 @@ bool speculator::call(llama_tokens & curr, /* OUT */ size_t & n_matched)
     return false;
 }
 
-int speculator::loop()
+int loop(config conf)
 {
     using namespace std::chrono_literals;
 
+    llama_model_params model_params = llama_model_default_params();
+    model_params.n_gpu_layers       = conf.n_gpu_layers;
+
+    llama_model * model = llama_load_model_from_file(conf.model_path.c_str(), model_params);
+    if (model == nullptr)
+    {
+        return 1;
+    }
+
     zmq::context_t zmq_ctx(1);
     zmq::socket_t socket(zmq_ctx, ZMQ_REQ);
-    socket.connect(conf_.attach_to);
-    query_ctx_.client = &socket;
+    socket.connect(conf.attach_to);
 
     llama_context_params ctx_params = llama_context_default_params();
-    ctx_params.n_batch   = conf_.n_batch;
-    ctx_params.n_ctx     = conf_.n_ctx;
-    ctx_params.n_threads = conf_.n_threads;
+    ctx_params.n_batch   = conf.n_batch;
+    ctx_params.n_ctx     = conf.n_ctx;
+    ctx_params.n_threads = conf.n_threads;
 
-    llama_context * ctx = llama_new_context_with_model(model_, ctx_params);
-    llama_batch   batch = llama_batch_init(conf_.n_batch, 0, 1);
-    llama_tokens   curr; // empty 
+    llama_context * llama_ctx = llama_new_context_with_model(model, ctx_params);
+    llama_batch         batch = llama_batch_init(conf.n_batch, 0, 1);
+    
+    llama_tokens curr; // empty 
     size_t n_matched = 0;
 
     while (true)
     {
         // get work/reconcile
-        if (call(curr, n_matched))
+        if (call(&socket, curr, n_matched))
         {
             fprintf(stderr, "done.\n");
             n_matched = 0;
@@ -130,7 +90,7 @@ int speculator::loop()
             continue;
         }
 
-        llama_kv_cache_seq_rm(ctx, 0, n_matched, -1);
+        llama_kv_cache_seq_rm(llama_ctx, 0, n_matched, -1);
         if (n_matched == curr.size())
         {
             n_matched -= 1;
@@ -143,12 +103,12 @@ int speculator::loop()
         }
         batch.logits[batch.n_tokens - 1] = true;
 
-        if (llama_decode(ctx, batch) != 0)
+        if (llama_decode(llama_ctx, batch) != 0)
         {
             fprintf(stderr, "%s : failed to eval, return code %d\n", __func__, 1);
             continue;
         }
-        auto next_tokens = greedy_tokens(model_, ctx, batch.n_tokens - 1, batch.n_tokens);
+        auto next_tokens = greedy_tokens(model, llama_ctx, batch.n_tokens - 1, batch.n_tokens);
         if (next_tokens.size() != 1)
         {
             fprintf(stderr, "invalid next tokens size\n");
@@ -158,9 +118,10 @@ int speculator::loop()
         curr.push_back(next_tokens[0]);
     }
 
-    llama_batch_free(query_ctx_.batch);
-    llama_free(query_ctx_.llama_ctx);
+    llama_batch_free(batch);
+    llama_free(llama_ctx);
     socket.close();
+    llama_free_model(model);
     return 0;
 }
 
@@ -184,16 +145,10 @@ int main(int argc, char ** argv)
     parser p;
     p.parse_options(argc, argv, conf);
 
-    auto sp = speculator::create(conf);
-    if (sp == nullptr)
-    {
-        fprintf(stderr, "Unable to create speculator\n");
-        res = 1;
-    }
-    else
-    {
-        res = sp->loop();
-    }
+
+
+    res = loop(conf);
+    
 
     llama_backend_free();
 
