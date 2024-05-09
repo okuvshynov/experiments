4fb14570266bcac62a59accea510a81aa489a00a
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2024-05-09 16:17:44 -0400

    remove unused

--- dlm/dlm.cpp
+++ dlm/dlm.cpp
@@ -11,11 +11,44 @@
 #include <httplib.h>
 
 #include "config.h"
-#include "query_context.h"
 #include "utils.h"
 
 namespace
 {
+
+using llama_tokens = std::vector<llama_token>;
+
+struct query
+{
+    std::string prompt;
+    size_t      n_predict; // new tokens to produce
+};
+
+// mutable part of context which might be accessed by 
+// multiple threads. Protected by single mutex for simplicify.
+struct spec_context
+{
+    llama_tokens speculation;
+    bool done = false;
+    std::mutex mtx;
+};
+
+struct output_context
+{
+    bool done = false;
+    std::string             output;
+    std::mutex              mtx;
+};
+
+struct query_context
+{
+    query           q;
+    llama_context * llama_ctx;
+    llama_batch     batch;
+    spec_context    spec_ctx;
+    size_t          n_len;
+    output_context  out_ctx;
+};
 
 struct config
 {
