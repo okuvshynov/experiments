67896638f17458d454f684978811799d6d4dbc28
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2024-07-02 21:59:49 -0400

    bbb: prefix

--- llm_beam_width/beam_width.cpp
+++ llm_beam_width/beam_width.cpp
@@ -8,6 +8,17 @@
 #include <common.h>
 #include <llama.h>
 
+/*
+ * First, we need to generate in/out pairs.
+ * 1. Prepare template (as we are going to use instruct)
+ * 2. Prepare prompts (e.g. 100-1000 prompts?)
+ * 3. Merge them into in[id].txt
+ * 4. For each in[id].txt write corresponding out[id].tokens and out[id].txt. Tokens would be a list of ints
+ *
+ * 5. Then, run smaller model on same inputs. While doing that, measure the beam width effect on predicting main model output.
+ */
+
+
 int main(int argc, char ** argv)
 {
     gpt_params params;
