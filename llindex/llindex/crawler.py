aea3b2a425230a037a07ab26223b6df9f0ac7c5f
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2024-10-04 12:43:09 -0400

    llindex: support git ls-files

--- llindex/llindex/crawler.py
+++ llindex/llindex/crawler.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import subprocess
 import fnmatch
 
 from typing import List, Dict, Any, Tuple
@@ -9,6 +10,25 @@ FileEntry = Dict[str, Any]
 Index = Dict[str, FileEntry]
 FileEntryList = List[FileEntry]
 
+def gen_walk(directory):
+    for root_path, _, files in os.walk(directory):
+        for file in files:
+            full_path = os.path.join(root_path, file)
+            yield os.path.relpath(full_path, directory)
+
+def get_git_files(directory):
+    try:
+        result = subprocess.run(['git', '-C', directory, 'ls-files'], 
+                                capture_output=True, text=True, check=True)
+        return result.stdout.splitlines()
+    except subprocess.CalledProcessError:
+        logging.error(f"{directory} is not a valid Git repository.")
+        return []
+
+traverse = {
+    'git': get_git_files,
+    'walk': gen_walk
+}
 
 class Crawler:
     def __init__(self, root, conf: Dict[str, any]):
@@ -24,6 +44,16 @@ class Crawler:
         else:
             self.excludes = []
 
+        self.traverse = get_git_files
+
+        if 'traverse' in conf:
+            if conf['traverse'] in traverse:
+                self.traverse = traverse[conf['traverse']]
+            else:
+                logging.warning(f'unknown traversal method: {conf["traverse"]}. Supported values: {traverse.keys()}')
+                logging.warning(f'fallback to plain walk')
+                self.traverse = gen_walk
+
     def should_process(self, path):
         included = any(fnmatch.fnmatch(path, p) for p in self.includes)
         excluded = any(fnmatch.fnmatch(path, p) for p in self.excludes)
@@ -32,20 +62,18 @@ class Crawler:
     def run(self, prev_index):
         result = []
         reused = []
-        for root_path, _, files in os.walk(self.root):
-            for file in files:
-                full_path = os.path.join(root_path, file)
-                relative_path = os.path.relpath(full_path, self.root)
-                if self.should_process(relative_path):
-                    logging.info(f'processing {relative_path}')
-                    file_info = get_file_info(full_path, self.root)
-                    if file_info is None:
-                        continue
-                    if relative_path in prev_index and prev_index[relative_path]["checksum"] == file_info["checksum"]:
-                        # Reuse previous result if checksum hasn't changed
-                        reused.append(prev_index[relative_path])
-                    else:
-                        result.append(file_info)
+        for relative_path in self.traverse(self.root):
+            if self.should_process(relative_path):
+                logging.info(f'processing {relative_path}')
+                full_path = os.path.join(self.root, relative_path)
+                file_info = get_file_info(full_path, self.root)
+                if file_info is None:
+                    continue
+                if relative_path in prev_index and prev_index[relative_path]["checksum"] == file_info["checksum"]:
+                    # Reuse previous result if checksum hasn't changed
+                    reused.append(prev_index[relative_path])
                 else:
-                    logging.info(f'skipping {relative_path}')
+                    result.append(file_info)
+            else:
+                logging.info(f'skipping {relative_path}')
         return result, reused
